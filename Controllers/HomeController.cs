3419d8f2b588bab0c482ffbc633ee714151139da
muhammed nirjas <nirjasmuhammed@gmail.com>
2024-05-12 14:13:43 +0530

    test commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace pipelines_dotnet_core.Controllers
     {
         public IActionResult Index()
         {
+            //test committing and pipeline
             return View();
         }
 
